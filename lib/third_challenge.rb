f18e3974fe6c8372180bc7aabc3b1056904447ee
braj011 <bhagyaraj01@gmail.com>
2018-08-21 15:28:51 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,7 +17,7 @@ def third_challenge
       :hero_friends => []
    }, 
    :capulet => {
-     :patriarch => {
+      :patriarch => {
         :name => "Lord Capulet",
         :age => "50"
       },
